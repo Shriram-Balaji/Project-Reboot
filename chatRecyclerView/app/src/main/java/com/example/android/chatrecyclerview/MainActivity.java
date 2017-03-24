8b2b7eb98c598bd9a8b3f85e3b5b5b49bb5fc4d3
Harold Prabhu <herald.prabu97@live.com>
2017-03-24 22:51:53 +0530

    Dynamic recyclerView with text sending

--- chatRecyclerView/app/src/main/java/com/example/android/chatrecyclerview/MainActivity.java
+++ chatRecyclerView/app/src/main/java/com/example/android/chatrecyclerview/MainActivity.java
@@ -4,20 +4,37 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    String messages[]={"hello","hi","how are you?","i am fine"};
-    boolean sent[]={true,false,true,false};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final ArrayList<Boolean> sent=new ArrayList<Boolean>();
+        final ArrayList<String> messages=new ArrayList<String>();
+        messages.add("hi");
+        sent.add(false);
         RecyclerView recyclerView= (RecyclerView) findViewById(R.id.recycler_view);
-        RecyclerAdapter adapter=new RecyclerAdapter(sent,messages);
-        LinearLayoutManager layoutManager=new LinearLayoutManager(this);
+        final RecyclerAdapter adapter=new RecyclerAdapter(sent,messages);
+        LinearLayoutManager layoutManager=new LinearLayoutManager(MainActivity.this);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setHasFixedSize(true);
         recyclerView.setAdapter(adapter);
+        Button send=(Button)findViewById(R.id.sendButton);
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText msg=(EditText)findViewById(R.id.messageEditText);
+                messages.add(msg.getText().toString());
+                sent.add(true);
+                adapter.notifyDataSetChanged();
+            }
+        });
     }
 }
--- chatRecyclerView/app/src/main/java/com/example/android/chatrecyclerview/RecyclerAdapter.java
+++ chatRecyclerView/app/src/main/java/com/example/android/chatrecyclerview/RecyclerAdapter.java
@@ -6,14 +6,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 /**
  * Created by Harold Prabhu on 17-03-2017.
  */
 
 class RecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
-    boolean[] isSender;
-    String[] messages;
-    public RecyclerAdapter(boolean[] isSender, String[] messages) {
+    ArrayList<Boolean> isSender;
+    ArrayList<String> messages;
+    public RecyclerAdapter(ArrayList<Boolean> isSender, ArrayList<String> messages) {
           this.isSender=isSender;
           this.messages=messages;
     }
@@ -35,14 +37,14 @@ class RecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-        if(isSender[position]){
+        if(isSender.get(position)){
             ViewHolderRight viewHolderRight =(ViewHolderRight)holder;
-            viewHolderRight.SentMsgTx.setText(messages[position]);
+            viewHolderRight.SentMsgTx.setText(messages.get(position));
 
         }
         else{
             ViewHolderLeft viewHolderLeft=(ViewHolderLeft)holder;
-            viewHolderLeft.ReceivedMsgTx.setText(messages[position]);
+            viewHolderLeft.ReceivedMsgTx.setText(messages.get(position));
 
         }
 
@@ -76,12 +78,12 @@ class RecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
     public int getItemViewType(int position) {
         // Just as an example, return 0 or 2 depending on position
         // Note that unlike in ListView adapters, types don't have to be contiguous
-        if(isSender[position])
+        if(isSender.get(position))
           return 1;
         return 0;
     }
     @Override
     public int getItemCount() {
-        return messages.length;
+        return messages.size();
     }
 }
